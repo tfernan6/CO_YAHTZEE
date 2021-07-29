ad846867b9fecef543c57c7ca7bbac0b4d80b04b
mchoffman <85776653+mchoffman@users.noreply.github.com>
2021-07-28 19:43:51 -0600

    Added username to new chat messages

--- Assets/Scripts/Chat/ChatController.cs
+++ Assets/Scripts/Chat/ChatController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,7 +21,8 @@ public class ChatController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        username = OldLogin.playerName;
+        //username = OldLogin.playerName;
+        username = PhotonNetwork.NickName;
     }
 
     // Update is called once per frame
