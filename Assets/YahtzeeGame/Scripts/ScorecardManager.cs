9d2a0b88887a2f419566761fa91e14919ebf9927
Omar Ismail <54246952+omarismail97@users.noreply.github.com>
2021-07-29 22:37:10 -0400

    Added Scoreboard for all 6 players

--- Assets/YahtzeeGame/Scripts/ScorecardManager.cs
+++ Assets/YahtzeeGame/Scripts/ScorecardManager.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScorecardSize : MonoBehaviour
+public class ScorecardManager : MonoBehaviour
 {
+    
+    // For testing purposes, will grab this from GameManager
+    public int numberOfPlayers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void populateScoreboard()
     {
         
     }
