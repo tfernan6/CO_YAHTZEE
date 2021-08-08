99f40abf5d1c4ffc46b636551552799c394f45ce
MJiang18 <milesjiang18@gmail.com>
2021-08-08 14:41:49 -0400

    fixed dice

--- Assets/YahtzeeGame/Scripts/GameManager.cs
+++ Assets/YahtzeeGame/Scripts/GameManager.cs
@@ -339,8 +339,8 @@ namespace edu.jhu.co
             //PlayerList.text += LocalPlayer.NickName + " (" + LocalPlayer.GetScore() + ")" + System.Environment.NewLine;
             
             //update the player list in the scoreboard
-            PlayerNames.text = "";
-            Player LocalPlayer = PhotonNetwork.LocalPlayer;
+            //PlayerNames.text = "";
+            LocalPlayer = PhotonNetwork.LocalPlayer;
 
            //update the player list in chat dropdown
             Dropdown chatPlayerList = GameObject.Find("DropdownPlayers").GetComponent<Dropdown>();
